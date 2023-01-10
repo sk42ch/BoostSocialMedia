c69d0fe56ba2ae88b87309add2e998e6aa0a3fa2
Mohammad Sohel <51685354+sk42ch@users.noreply.github.com>
2023-01-10 10:11:32 -0500

    INstagram Follows/Likes added

--- automation/src/main/java/web/AddmefastMenu.java
+++ automation/src/main/java/web/AddmefastMenu.java
@@ -7,4 +7,6 @@ public interface AddmefastMenu {
     String YOUTUBE_SUBSCRIBE = "YouTube Subscribe";
     String FB_PAGE_LIKES = "FB Page Likes/Followers";
     String TWITTER_FOLLOWERS = "Twitter Followers";
+    String INSTAGRAM_FOLLOWERS ="Instagram Followers";
+    String INSTAGRAM_LIKES = "Instagram Likes";
 }
--- automation/src/test/java/addmefast/YT_Channel_101.java
+++ automation/src/test/java/addmefast/YT_Channel_101.java
@@ -5,11 +5,12 @@ import utils.DriverFactory;
 import web.Youtube;
 import web.YoutubeLike;
 
-import static web.AddmefastMenu.YOUTUBE_LIKES;
-import static web.AddmefastMenu.YOUTUBE_SUBSCRIBE;
+import static web.AddmefastMenu.*;
 
 public class YT_Channel_101 extends DriverFactory {
     private static final String STEP_1 = "yt_Subscribers";
+    private static final String STEP_2 ="yt_Like";
+    private static final String STEP_3="insta_Followers";
     Youtube youtube;
     YoutubeLike youtubeLike;
 
@@ -17,7 +18,7 @@ public class YT_Channel_101 extends DriverFactory {
     public void yt_Subscribers() {
         youtube = new Youtube();
         youtubeLike = youtube.select_Youtube_Channel_and_Navigate_to_AddmefastPage_("RanaRahman101").clickOn_Amf_Menu(YOUTUBE_SUBSCRIBE);
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 1; i++) {
             youtubeLike.Youtube_Subscribe_Flow();
         }
     }
@@ -25,8 +26,24 @@ public class YT_Channel_101 extends DriverFactory {
     @Test(dependsOnMethods = STEP_1, alwaysRun = true)
     public void yt_Like() {
         youtubeLike = youtube.clickOn_Amf_Menu(YOUTUBE_LIKES);
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 1; i++) {
             youtubeLike.Youtube_Like_Flow();
+        }
+    }
+
+    @Test(dependsOnMethods = STEP_2, alwaysRun = true)
+    public void insta_Followers() {
+        youtubeLike = youtube.clickOn_Amf_Menu(INSTAGRAM_FOLLOWERS);
+        for (int i = 0; i < 2; i++) {
+            youtubeLike.Instagram_Followers_Flow();
+        }
+    }
+
+    @Test(dependsOnMethods = STEP_3, alwaysRun = true)
+    public void insta_Likes() {
+        youtubeLike = youtube.clickOn_Amf_Menu(INSTAGRAM_LIKES);
+        for (int i = 0; i < 2; i++) {
+            youtubeLike.Instagram_Likes_Flow();
         }
     }
 }
